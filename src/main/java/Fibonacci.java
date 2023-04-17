a8d70f9d2fc688643080475c6e69d6e7c256e877
Kung Lo <professionally_unemployed@proton.me>
2023-04-17 11:24:48 +0000

    Passed

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -15,6 +15,9 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        return fib(n - 1) + fib(n - 2);
     }
 }
